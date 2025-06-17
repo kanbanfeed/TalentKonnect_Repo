1494898e16cbbe8cfdd906544665d40801337e89
Praneshxo <praneshgara@gmail.com>
2025-06-17 12:15:35 +0530

    vite-issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Ensures correct asset paths
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
