4a362d68f79f69e10c009abe93de095e7c4f1e6a
ProfessionalProgrammer95 <rakshithav1358@gmail.com>
2025-08-20 14:45:59 +0530

    updating

--- api/raffle/tickets/[userId].ts
+++ api/raffle/tickets/[userId].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getTickets } from '../../_store.ts';
+import { getTickets } from '../../_store';
 
 
 function normalizeUserId(q: unknown): string {
